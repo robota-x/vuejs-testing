cfad8ec575859fd02a3601f5f43126865014bc57
lorenzoturrino <lorenzo.turrino@gmail.com>
2016-12-28 10:18:28 +0000

    add contact app and write some models

---
+++ contact/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.utils.text import Truncator
+
+
+class EnquiryType(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, blank=True, null=True)
+
+
+class Enquiry(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    email = models.EmailField(max_length=255, blank=True, null=True)
+    phone = models.CharField(max_length=255, blank=True, null=True)
+    other_contact = models.TextField(blank=True, null=True)
+
+    text = models.TextField()
+    type = models.ForeignKey(EnquiryType)
+
+    def __str__(self):
+        return '{0} {1}: {2}'.format(self.first_name, self.last_name, Truncator(self.text).chars(50))
