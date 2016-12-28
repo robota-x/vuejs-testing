76471913002af6c3b57bae71245226433ed58625
lorenzoturrino <lorenzo.turrino@gmail.com>
2016-12-28 12:59:51 +0000

    add vanilla google maps

--- vuesample/contact/static/js/contact.js
+++ vuesample/contact/static/js/contact.js
@@ -1,10 +1,24 @@
 'use strict';
-
 console.log('contact loaded');
 
+// gmaps callback
+function initMap() {
+    var myPosition = {lat: 42, lng: 42};
+    var map = new google.maps.Map(document.getElementById('office-map'), {
+        zoom: 4,
+        center: myPosition
+    });
+    var marker = new google.maps.Marker({
+        position: myPosition,
+        map: map
+    });
+}
+
+
+// vue.js
 window.onload = function() {
     var app = new Vue({
-        el: '#office-map',
+        el: '#office-list',
         data: {
             message: 'Hello Vue!'
         }
