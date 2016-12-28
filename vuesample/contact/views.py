4e36ffb1b22dc57261c3e8e831ad86596e89cfc9
lorenzoturrino <lorenzo.turrino@gmail.com>
2016-12-28 11:41:05 +0000

    tweak view, add vue.js script

--- vuesample/contact/views.py
+++ vuesample/contact/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import JsonResponse
 from django.shortcuts import render
 
 from . import forms
@@ -7,12 +7,13 @@ from . import forms
 def index(request):
     form = forms.EnquiryForm(request.POST or None)
 
+    # deal with vue posting the data
     if request.POST:
         if form.is_valid():
             form.save()
-            return HttpResponse(status=201)
+            return JsonResponse(status=201, data={})
         else:
-            return None
+            return JsonResponse(status=400, data=form.errors)
 
     return render(request, 'contact/index.html', {'form': form})
 
