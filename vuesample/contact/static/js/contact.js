f900a000a5dc8ac4b7bdd8e89d841ad2f45d8006
lorenzoturrino <lorenzo.turrino@gmail.com>
2016-12-28 12:10:57 +0000

    integrate vue in template

---
+++ vuesample/contact/static/js/contact.js
@@ -0,0 +1,13 @@
+'use strict';
+
+console.log('contact loaded');
+
+window.onload = function() {
+    var app = new Vue({
+        el: '#office-map',
+        data: {
+            message: 'Hello Vue!'
+        }
+    });
+};
+
